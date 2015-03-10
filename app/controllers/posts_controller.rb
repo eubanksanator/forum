f7ddc20c2c8d8b97ab908ab4aa2910feab797269
Travis Eubanks <TRAVIS@traviss-air.attlocal.net>
2015-03-10 11:54:26 -0500

    Revert a132461..d4eb167
    This rolls back to commit a132461f4d90ec6cddf84c2ba7f917ea2803113d.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,12 +46,12 @@ class PostsController < ApplicationController
   end
 
 
-                                  private
-                                    def post_params
-                                      params.require(:post).permit(:title, :content)
-                                    end
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 
-                                    def find_post
-                                      @post = Post.find(params[:id])
-                                    end
+    def find_post
+      @post = Post.find(params[:id])
+    end
 end
