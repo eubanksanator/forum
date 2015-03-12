8526b1ad648449fd4cb261038f711e96777b9bef
Travis Eubanks <TRAVIS@traviss-air.attlocal.net>
2015-03-12 15:02:16 -0500

    adds comments destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,21 @@ class CommentsController < ApplicationController
     #takes newely created comment.id and matches it with post_id in the Post model
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:comment))
+    @comment.user_id = current_user.id
 
     if @comment.save
       redirect_to post_path(@post)
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
   end
 
 end
