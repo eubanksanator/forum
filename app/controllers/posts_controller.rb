726433c6a6caf4a3493e92563c39c2e3aed4e12a
Travis Eubanks <TRAVIS@Traviss-MacBook-Air.local>
2015-03-12 08:41:14 -0500

    adds authenticate_user before new_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @posts = Post.all.order('created_at DESC')
@@ -11,7 +12,7 @@ class PostsController < ApplicationController
 
 
   def new
-    @post = current_user.posts.build(post_params)
+    @post = current_user.posts.new
   end
 
 
